d22d8317f97cf5c4c0ebd49661b7d9bd7ccb9c84
Jan Pabich <janekp923@gmail.com>
2024-10-09 22:59:09 +0200

    logowanie4

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-testtest1
-
-autor jan p 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP site</title>
+</head>
+<body>
+    LOGIN site
+    <form>
+    Login <br><input type="text" name="login"> <br>
+    Login <br><input type="password" name="password"> <br><br>
+    <input type="submit" value="Log in">
+    
+    </form>
+</body>
+</html>
