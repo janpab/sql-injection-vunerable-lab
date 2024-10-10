4fd15370be7b11b5b36ffff96191e4e96532addc
Jan Pabich <janekp923@gmail.com>
2024-10-10 21:24:18 +0200

    index php change

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        Login: <br> <input type="text" name="login"> <br>
+        Password: <br> <input type="password" name="password" > <br><br>
+        <input type="submit" value="log in" > <br><br>
+    </form>
+    
+</body>
+</html>
